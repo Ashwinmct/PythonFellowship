f296d679accbecce8811c14c2dc6184363be0c0a
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-09 22:06:20 +0530

    Refactored. HelloWorld feature to say hello with name

--- hello_world/HelloWorld.py
+++ hello_world/HelloWorld.py
@@ -3,14 +3,20 @@ import unittest
 
 class HelloWorld:
 
-    def get_hello_world(self):
-            return "Hello World"
+    def get_hello_world(self,name=None):
+        if name is None:
+            return "Hello World !"
+        return "Hello World ! I'm "+name
 
 
 class HelloWorldTest(unittest.TestCase):
     def test_for_get_hello_world_method(self):
         hello_note = HelloWorld()
-        self.assertEqual(hello_note.get_hello_world(), "Hello World")
+        self.assertEqual(hello_note.get_hello_world(), "Hello World !")
+
+    def test_for_get_hello_world_method_name(self):
+        hello_note = HelloWorld()
+        self.assertEqual(hello_note.get_hello_world("Ashwin"), "Hello World ! I'm Ashwin")
 
 
 if __name__ == '__main__':
