6af3f2e6a224b409d73197b73abc1df68ae76601
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 00:57:41 +0530

    Added. feature to find singlet combination

--- FlipCoin.py
+++ FlipCoin.py
@@ -3,15 +3,21 @@ import random
 
 class FlipCoin:
 
-#flip a coin and return 'H' for heads and 'T' for tails
-   def simulateFlip(self,flipping_count=None):
-       if (flipping_count is None):
-           return random.choice(['H', 'T'])
-       flipping_result=''
-       for flip in range(flipping_count):
-           flipping_result+=random.choice(['H','T'])
-       return flipping_result
+    def simulate_flip(self, flipping_count=None):
+        if (flipping_count is None):
+            return random.choice(['H', 'T'])
+        flipping_result = ''
+        for flip in range(flipping_count):
+            flipping_result += random.choice(['H', 'T'])
+        return flipping_result
+
+    def simulate_single_flip(self, count):
+        singlet_combination = {'H': 0, 'T': 0}
+        for term in range(count):
+            combination = self.simulate_flip()
+            singlet_combination[combination] = singlet_combination.get(combination) + 1
+        return singlet_combination
 
 
 flip_coin = FlipCoin()
-print(flip_coin.simulateFlip(5))
+print(flip_coin.simulate_single_flip(5))
