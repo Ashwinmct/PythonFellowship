fa383ed55eb5286e8c26b0dc3ec26f553b6f4317
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 00:34:47 +0530

    Refactored. coin fliping feature with count

--- FlipCoin.py
+++ FlipCoin.py
@@ -4,14 +4,14 @@ import random
 class FlipCoin:
 
 #flip a coin and return 'H' for heads and 'T' for tails
-   def simulateFlip(self):
-       return random.choice(['H', 'T'])
+   def simulateFlip(self,flipping_count=None):
+       if (flipping_count is None):
+           return random.choice(['H', 'T'])
+       flipping_result=''
+       for flip in range(flipping_count):
+           flipping_result+=random.choice(['H','T'])
+       return flipping_result
 
 
 flip_coin = FlipCoin()
-print(flip_coin.simulateFlip())
-
-
-
-
-
+print(flip_coin.simulateFlip(5))
