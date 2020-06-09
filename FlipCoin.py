403d850c247e3b3cf81c6f35d7870234d5af55d8
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 01:29:11 +0530

    Refactored. seperate combinational flips with common method

--- FlipCoin.py
+++ FlipCoin.py
@@ -9,27 +9,15 @@ class FlipCoin:
             flipping_result += random.choice(['H', 'T'])
         return flipping_result
 
-    def simulate_single_flip(self, count):
-        singlet_combination = {'H': 0, 'T': 0}
-        for term in range(count):
-            combination = self.simulate_flip(1)
-            singlet_combination[combination] = singlet_combination.get(combination) + 1
-        return singlet_combination
 
-    def simulate_doublet_flip(self, count):
-        doublet_combination = {'HH': 0, 'TT': 0, 'HT': 0, 'TH': 0}
+    def simulate_combinational_flip(self, count):
+        flipping_combinations={'H': 0, 'T': 0, 'HH': 0, 'TT': 0, 'HT': 0, 'TH': 0,
+                              'HHH': 0, 'HHT': 0, 'TTT': 0, 'TTH': 0, 'HTH': 0, 'HTT': 0, 'THT': 0, 'THH': 0}
         for term in range(count):
-            combination = self.simulate_flip(2)
-            doublet_combination[combination] = doublet_combination.get(combination) + 1
-        return doublet_combination
-
-    def simulate_triplet_flip(self, count):
-        triplet_combination = {'HHH': 0, 'HHT': 0, 'TTT': 0, 'TTH': 0, 'HTH': 0, 'HTT': 0, 'THT': 0, 'THH': 0}
-        for term in range(count):
-            combination = self.simulate_flip(3)
-            triplet_combination[combination] = triplet_combination.get(combination) + 1
-        return triplet_combination
+            combination = self.simulate_flip(random.choice([1,2,3]))
+            flipping_combinations[combination] = flipping_combinations.get(combination) + 1
+        return flipping_combinations
 
 
 flip_coin = FlipCoin()
-print(flip_coin.simulate_triplet_flip(5))
+print(flip_coin.simulate_combinational_flip(24))
