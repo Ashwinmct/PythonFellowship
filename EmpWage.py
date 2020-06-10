f8ba3a62bdfa02f368846f0f3c030262edce7e46
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 11:32:55 +0530

    Added. feature to calculate employee wage for given days

--- EmpWage.py
+++ EmpWage.py
@@ -4,12 +4,15 @@ PAY_PER_HOUR = 200
 FULL_DAY_PRESENT = 2
 HALF_A_DAY_PRESENT = 1
 ABSENT_FOR_THE_DAY = 0
+total_salary = 0
 
-
-def calculate_employee_wage_of_the_day():
+def calculate_employee_wage_of_the_day(days):
+	global total_salary
 	working_hours = {ABSENT_FOR_THE_DAY: 0, HALF_A_DAY_PRESENT: 4, FULL_DAY_PRESENT: 8}
-	attendance_status = random.choice([FULL_DAY_PRESENT, HALF_A_DAY_PRESENT, ABSENT_FOR_THE_DAY])
-	return PAY_PER_HOUR*working_hours.get(attendance_status)
+	for day in range(days):
+		attendance_status = random.choice([FULL_DAY_PRESENT, HALF_A_DAY_PRESENT, ABSENT_FOR_THE_DAY])
+		total_salary+=PAY_PER_HOUR*working_hours.get(attendance_status)
+	return total_salary
 
 
-print("Employee salary is = ", calculate_employee_wage_of_the_day())
+print("Employee salary for 30 days is = ", calculate_employee_wage_of_the_day(30))
