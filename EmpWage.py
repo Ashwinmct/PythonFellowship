e789d924518d3b96f381fffb67d0d13a873b8210
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 13:04:34 +0530

    Refactored. employee wage calculation for given days and given working hours

--- EmpWage.py
+++ EmpWage.py
@@ -6,13 +6,18 @@ HALF_A_DAY_PRESENT = 1
 ABSENT_FOR_THE_DAY = 0
 total_salary = 0
 
-def calculate_employee_wage_of_the_day(days):
+def calculate_employee_wage_of_the_day(max_working_days, max_working_hours):
 	global total_salary
-	working_hours = {ABSENT_FOR_THE_DAY: 0, HALF_A_DAY_PRESENT: 4, FULL_DAY_PRESENT: 8}
-	for day in range(days):
+	total_hours_worked = 0
+	working_hours_details = {ABSENT_FOR_THE_DAY: 0, HALF_A_DAY_PRESENT: 4, FULL_DAY_PRESENT: 8}
+	for day in range(max_working_days):
 		attendance_status = random.choice([FULL_DAY_PRESENT, HALF_A_DAY_PRESENT, ABSENT_FOR_THE_DAY])
-		total_salary+=PAY_PER_HOUR*working_hours.get(attendance_status)
+		current_day_working_hour = working_hours_details.get(attendance_status)
+		total_salary += PAY_PER_HOUR*current_day_working_hour
+		total_hours_worked += current_day_working_hour
+		if total_hours_worked > max_working_hours:
+			break
 	return total_salary
 
 
-print("Employee salary for 30 days is = ", calculate_employee_wage_of_the_day(30))
+print("Employee salary for 30 days is = ", calculate_employee_wage_of_the_day(30, 50))
