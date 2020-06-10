677687e71b28c7340f4b5fce32456cf411e084c1
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 22:28:31 +0530

    Added. example of pure function

--- functions_example.py
+++ functions_example.py
@@ -1,4 +1,4 @@
-#Higher order function : fuction with other function as parameter
+#Higher order function : function with other function as parameter
 def apply_thrice(function,argument):
 	return function(function(function(argument)))
 
@@ -6,6 +6,12 @@ def apply_thrice(function,argument):
 def multiply_by_two(number):
 	return 2 * number
 
+#pure_function : function which is only depend on arguments given
+def pure_function_example(number1, number2):
+	return number1+number2
+
+
 #driver code
-num=int(input("Enter the no. "))
+num=int(input("Enter one no. "))
 print("value", apply_thrice(multiply_by_two, num))
+print("added value = ", pure_function_example(int(input("Enter another number : ")), num))
