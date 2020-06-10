dd0e6cb96aa365c21fb1c31e96c3d9117180ecfe
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-10 10:37:06 +0530

    Added. feature to calculate salary of the day

--- EmpWage.py
+++ EmpWage.py
@@ -1,9 +1,17 @@
 import random
 
+salary = 0
+WORKING_HOURS = 8
+PAY_PER_HOUR = 200
+
 
 def check_employee_attendance():
-	if(random.randint(0,9) % 2 == 0):
+	if (random.randint(0, 9) % 2 == 0):
+		global salary
+		salary = WORKING_HOURS * PAY_PER_HOUR
 		return "Employee is Present"
-	return "Employee is abscent"
+	return "Employee is absent"
+
 
 print(check_employee_attendance())
+print("Employee salary is = ", salary)
