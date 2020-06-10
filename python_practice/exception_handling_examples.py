c125563f5d828f71d9b420cf0555da63b589f23d
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-11 00:16:42 +0530

    Added. example for exception handling in function

--- python_practice/exception_handling_examples.py
+++ python_practice/exception_handling_examples.py
@@ -1,8 +1,12 @@
 #exception handling is used to handle exception as well as raise error
 #except : used to catch any exception
 
-try:
-	5/0
-except:
-	print("an error occured")
-	raise
+def divide(number1, number2):
+	try:
+		result = number1 / number2
+	except ZeroDivisionError:
+		print("Divided by zero")
+	finally:
+		return result
+
+print("divided value = ", divide(int(input("Enter a number ")), int(input("Enter another number "))))
