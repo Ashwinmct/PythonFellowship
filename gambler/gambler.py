1c28f4a3f91658d6ff22870ff138aa890411ec90
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-11 12:21:18 +0530

    Refactored. gambling simulation to do no. of times

--- gambler/gambler.py
+++ gambler/gambler.py
@@ -6,17 +6,19 @@ empty_pocket = 0
 is_win = 1
 
 
-def simulate_gambling():
-	money = stake
-	while goal > money > empty_pocket:
-		gamble = random.choice([0, 1])
-		if gamble == is_win:
-			money += 1
-		else:
-			money -= 1
+def simulate_gambling(times):
+	global money
+	for term in range(times):
+		money = stake
+		while goal > money > empty_pocket:
+			gamble_status = random.choice([0, 1])
+			if gamble_status == is_win:
+				money += 1
+			else:
+				money -= 1
 	if money == goal:
 		return "YOU WON"
 	return "YOU LOST"
 
 
-print(simulate_gambling())
+print(simulate_gambling(int(input("Enter no of times you want play gambling "))))
