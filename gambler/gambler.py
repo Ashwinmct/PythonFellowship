bc2277d410ca1676c3bbed25690c5234b5da6218
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-11 23:42:20 +0530

    Add. feature to simulate for next month only if win in current month

--- gambler/gambler.py
+++ gambler/gambler.py
@@ -16,18 +16,18 @@ def gamble(gambling_amount, dead_line, goal):
 
 def print_monthly_report(operation, status, func):
 	day_wise_details = dict(filter(lambda day: operation(day[1], 0), gambling_results_by_day.items()))
-	print("Total days you %s : %d, Days you %s are " % (status,len(day_wise_details), status), day_wise_details)
-	print("Day you %s mostly " %(status), func(gambling_results_by_day.items(), key=operator.itemgetter(1))[0])
+	print("Total days you %s : %d, Days you %s are " % (status, len(day_wise_details), status), day_wise_details)
+	print("Day you %s mostly " % status, func(gambling_results_by_day.items(), key=operator.itemgetter(1))[0])
 
 
-def print_result():
+def print_monthly_result():
 	total_money_loss_or_won = sum(gambling_results_by_day.values())
 	if total_money_loss_or_won > 0:
 		print("You WON by ", total_money_loss_or_won)
 	else:
-		print('You LOSS by ', total_money_loss_or_won)
-	print_monthly_report(operator.__gt__, "won",max)
-	print_monthly_report(operator.__le__, "loss",min)
+		print('You LOST by ', total_money_loss_or_won)
+	print_monthly_report(operator.__gt__, "won", max)
+	print_monthly_report(operator.__le__, "lost", min)
 
 
 def simulate_gambling(days):
@@ -39,12 +39,14 @@ def simulate_gambling(days):
 		money_low_limit = 0.5 * money
 		money = gamble(money_before_gamble, money_low_limit, money_hi_limit)
 		gambling_results_by_day[day + 1] = money - money_before_gamble
-	return print_result()
+	print_monthly_result()
+	if sum(gambling_results_by_day.values()) > 0:
+		return simulate_gambling_for_month()
 
 
-def simulate_gambling_for_a_month():
+def simulate_gambling_for_month():
 	simulate_gambling(30)
 
 
 #driving code
-simulate_gambling_for_a_month()
+simulate_gambling_for_month()
