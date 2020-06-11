b7c51861e9f6812b389a27b5bf3354a13c6b40e8
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-12 02:50:25 +0530

    Added. feature to set values for user and computer

--- tictactoe/tic_tac_toe_game.py
+++ tictactoe/tic_tac_toe_game.py
@@ -1,8 +1,14 @@
+import random
+
 
 class TicTacToe:
 	tic_tac_toe_board = [['', '', ''], ['', '', ''], ['', '', '']]
-
+	user = ''
+	computer = ''
+	
+	
 	def __init__(self):
+		self.initialise_game()
 		self.play_game()
 
 	def reset_board(self):
@@ -11,7 +17,6 @@ class TicTacToe:
 
 	def play_game(self):
 		self.reset_board()
-		print("Welcome to the game")
 		self.print_board()
 
 	def print_board(self):
@@ -19,6 +24,24 @@ class TicTacToe:
 		print("Your Board")
 		for row in range(len(tic_tac_toe_board)):
 			print("%s || %s || %s" % (tic_tac_toe_board[row][0], tic_tac_toe_board[row][1], tic_tac_toe_board[row][2]))
+
+	def initialise_game(self):
+		print("Welcome to the game \nBoard Layout: \n 1 || 2 || 3 \n 4 || 5 || 6 \n 7 || 8 || 9")
+		print("Enter position you want as shown as above")
+		self.set_values()
+
+
+	def set_values(self):
+		global user, computer
+		if random.randint(0,1) == 0:
+			print("You will be 'O' \n Computer will be 'X' ")
+			computer = str('x')
+			user = str('o')
+		else:
+			print("You will be 'X' \n Computer will be 'O' ")
+			computer = str('o')
+			user = str('x')
+
 
 
 #driver code
