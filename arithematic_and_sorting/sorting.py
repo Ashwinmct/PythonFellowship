56e33749f833c2af4fc7071c89f437a3dac0ae07
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-13 11:16:24 +0530

    [ASHWIN B]Add. feature to compute

--- arithematic_and_sorting/sorting.py
+++ arithematic_and_sorting/sorting.py
@@ -2,18 +2,15 @@ from utilities.Input import Input
 
 
 class Sorter:
-	def __init__(self):
-		Sorter.take_input()
+	def compute(self, number_1, number_2, number_3):
+		return number_1 + number_2 * number_3
 
-	@classmethod
-	def take_input(cls):
-		number1 = Input.get_input(int, "Enter 1st number ")
-		number2 = Input.get_input(int, "Enter 2nd number ")
-		number3 = Input.get_input(int, "Enter 3rd number ")
 
 
 #driver code:
 sorter = Sorter()
-
-
-
+number1 = Input.get_input(int, "Enter 1st number ")
+number2 = Input.get_input(int, "Enter 2nd number ")
+number3 = Input.get_input(int, "Enter 3rd number ")
+sorter.compute(number1,number2,number3)
+print("%d + %d * %d = "%(number1,number2,number3),sorter.compute(number1,number2,number3))
