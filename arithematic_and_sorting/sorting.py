d8473bd8251ca124d275bae01e1030480bc8326e
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-13 11:32:38 +0530

    [ASHWIN B]Refactor. compute method as generic

--- arithematic_and_sorting/sorting.py
+++ arithematic_and_sorting/sorting.py
@@ -1,9 +1,10 @@
+import operator
 from utilities.Input import Input
 
 
 class Sorter:
-	def compute(self, number_1, number_2, number_3):
-		return number_1 + number_2 * number_3
+	def compute(self, number1, number2, number3, operator1, operator2):
+		return operator1(number1, operator2(number2, number3))
 
 
 
@@ -12,5 +13,7 @@ sorter = Sorter()
 number1 = Input.get_input(int, "Enter 1st number ")
 number2 = Input.get_input(int, "Enter 2nd number ")
 number3 = Input.get_input(int, "Enter 3rd number ")
-sorter.compute(number1,number2,number3)
-print("%d + %d * %d = "%(number1,number2,number3),sorter.compute(number1,number2,number3))
+print("%d + ( %d * %d ) = " % (number1, number2, number3), sorter.compute(number1, number2, number3, operator.__add__, operator.__mul__))
+print("%d * ( %d + %d ) = " % (number1, number2, number3), sorter.compute(number1, number2, number3, operator.__mul__, operator.__add__))
+print("%d + ( %d / %d ) = " % (number1, number2, number3), sorter.compute(number1, number2, number3, operator.__add__, operator.__truediv__))
+print("%d mod ( %d + %d ) = " % (number1, number2, number3), sorter.compute(number1, number2, number3, operator.__mod__, operator.__add__))
