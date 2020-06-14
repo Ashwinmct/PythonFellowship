a53920ad22b0e7f900c0673a52223f6944fa98aa
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-14 16:54:23 +0530

    [ASHWIN B]Add. feature to convert binary string to standard nibble length

--- swapping_nibble/nibble_swap.py
+++ swapping_nibble/nibble_swap.py
@@ -3,15 +3,23 @@
 class BinaryOperations:
 	@staticmethod
 	def to_binary(number):
-		bin_num = ''
+		binary_string = ''
 		while number > 1:
-			bin_num = str(number % 2) + bin_num
+			binary_string = str(number % 2) + binary_string
 			number = number // 2
-		return '1' + bin_num
+		return '1' + binary_string
+
+	@classmethod
+	def convert_to_nibble(cls, number):
+		standard_nibble_length = 4
+		binary_number = BinaryOperations.to_binary(number)
+		if len(binary_number)%standard_nibble_length != 0:
+			binary_number = (standard_nibble_length - len(binary_number) % standard_nibble_length) * '0' + binary_number
+		return binary_number
 
 
 #driver code
 if __name__ == "__main__":
-	number = 55
-	binary_number = BinaryOperations.to_binary(number)
+	number = 1
+	binary_number = BinaryOperations.convert_to_nibble(number)
 	print(binary_number)
