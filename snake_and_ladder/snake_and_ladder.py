44ff2634f50d9485e12f9028f4628c1b396b2724
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-15 12:27:49 +0530

    [ASHWIN B]Refactor. get position method

--- snake_and_ladder/snake_and_ladder.py
+++ snake_and_ladder/snake_and_ladder.py
@@ -3,26 +3,27 @@ from utilities.Input import Input
 
 
 class SnakeAndLadder:
-	BOARD_STARTING = 1
-	BOARD_END = 100
-	game_status = True
+	__BOARD_STARTING = 1
+	__BOARD_END = 100
 
 	def __init__(self):
 		print("Welcome to SnakeAndLadder Game")
-		if Input.get_input("Enter 0 if you want to simulate snake_and_ladder_game", int) == 0:
+		if Input.get_input("Enter 0 if you want to simulate snake_and_ladder_game ", int) == 0:
 			self.__play_game()
 		print("Thank you")
 
 	def __play_game(self):
-		player1_position = self.BOARD_STARTING
-		player2_position = self.BOARD_STARTING
+		global player1_position, player2_position
+		game_status = True
+		player1_position = self.__BOARD_STARTING
+		player2_position = self.__BOARD_STARTING
 		dice_count = 0
-		while [self.game_status]:
+		while [game_status]:
 			player1_position = self.__get_position(player1_position)
 			dice_count += 1
 			player2_position = self.__get_position(player2_position)
 			dice_count += 1
-			if player1_position == self.BOARD_END or player2_position == self.BOARD_END:
+			if player1_position >= self.__BOARD_END or player2_position >= self.__BOARD_END:
 				break
 		print("Total dice rolls required: ", dice_count)
 
@@ -35,7 +36,7 @@ class SnakeAndLadder:
 	def __get_position(self, player_position):
 		dice_value = self.__get_dice_value()
 		player_position = player_position + dice_value
-		return self.BOARD_END if player_position >= self.BOARD_END else self.__check_option(player_position + dice_value)
+		return self.__BOARD_END if player_position >= self.__BOARD_END else self.__check_option(player_position)
 
 	def __check_option(self, position):
 		ladder_position = {1: 38, 4: 14, 8: 30, 21: 42, 28: 76, 50: 67, 71: 92, 80: 99}
