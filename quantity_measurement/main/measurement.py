838d12921f732e1918b64b2649b056ef7696ef3d
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-06-18 01:17:38 +0530

    [ASHWIN B]Refactor. equals method

--- quantity_measurement/main/measurement.py
+++ quantity_measurement/main/measurement.py
@@ -16,8 +16,10 @@ class Measurement(abc.ABC):
 			return False
 		if self.value == obj.value and self.unit == obj.unit:
 			return True
+		if self.convert_to_basic_unit_value(self) == self.convert_to_basic_unit_value(obj):
+			return True
 		return False
 
-
-
-
+	@staticmethod
+	def convert_to_basic_unit_value(obj):
+		return obj.value * obj.unit.get_basic_unit_conversion_factor()
--- quantity_measurement/main/measurement_units.py
+++ quantity_measurement/main/measurement_units.py
@@ -4,6 +4,9 @@ import enum
 class MeasurementUnit(enum.Enum):
 	class BaseUnit(enum.Enum):
 		LENGTH = 1
-	INCH = {"type": BaseUnit.LENGTH, "BasicUnitConversion": 1}
-	FEET = {"type": BaseUnit.LENGTH, "BasicUnitConversion": 12}
-	YARD = {"type": BaseUnit.LENGTH, "BasicUnitConversion": 36}
+	INCH = {"type": BaseUnit.LENGTH, "BasicUnitConversionFactor": 1}
+	FEET = {"type": BaseUnit.LENGTH, "BasicUnitConversionFactor": 12}
+	YARD = {"type": BaseUnit.LENGTH, "BasicUnitConversionFactor": 36}
+
+	def get_basic_unit_conversion_factor(self):
+		return self.value.get("BasicUnitConversionFactor")
